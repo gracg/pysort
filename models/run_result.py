a99f860eb7ba2ca14e12c21eb8a68ee6413bd57f
gracg <grac@gracg.top>
2022-09-10 14:18:46 -0400

    RunResult model added helper methods

--- models/run_result.py
+++ models/run_result.py
@@ -8,3 +8,14 @@ class RunResult:
     read_operations: int
     write_operations: int
     comparison_operations: int
+
+    def to_tuple(self):
+        return (self.name,self.size,self.read_operations,self.write_operations,self.comparison_operations)
+
+    @staticmethod
+    def labels():
+        return ['name','size','read_operations','write_operations','comparison_operations']
+
+    @staticmethod
+    def short_labels():
+        return ['name','size','reads','writes','comparisons']
